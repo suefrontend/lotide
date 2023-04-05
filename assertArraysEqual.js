be07ccdb57a8d36563ce025d62bdcfc503ebaf6d
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-04 21:08:15 -0700

    Complete assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function (arr1, arr2) {
+  let len = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+  for (let i = 0; i < len; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue;
+    } else if (arr1[i] !== arr2[i]) {
+      return;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // => should PASS
