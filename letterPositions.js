c7c5e09c36391d7cedcb3bae29ab98768949b5c0
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-05 19:16:02 -0700

    Complete letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const eqArrays = function (arr1, arr2) {
+  let len = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+  for (let i = 0; i < len; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue;
+    } else if (arr1[i] !== arr2[i]) {
+      return;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]); // => should PASS
