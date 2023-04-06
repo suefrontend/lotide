7f7a2a51638e562ce5bd09288431d8af2f3ba652
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-05 18:23:01 -0700

    Complete countLetters function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const result = {};
+
+  for (const char of str) {
+    if (char !== " ") {
+      if (result[char]) {
+        result[char] += 1;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const result = countLetters("lighthouse in the house");
+assertEqual(result.l, 1);
+assertEqual(result.l, 2);
