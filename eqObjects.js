6facb7df4c1f6295bda9e9a5edee4f6cc7d55df0
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-05 20:34:39 -0700

    Finish Step 3: Primitives As Values (Implementation)

---
+++ eqObjects.js
@@ -0,0 +1,39 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (const key of keys1) {
+    if (object1[key] === object2[key]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+// // => true
+console.log(eqObjects(shirtObject, anotherShirtObject));
+// console.log(assertEqual(eqObjects(shirtObject, anotherShirtObject)), true);
+
+const longSleeveShirtObject = {
+  size: "medium",
+  color: "red",
+  sleeveLength: "long",
+};
+console.log(eqObjects(shirtObject, longSleeveShirtObject));
+
+// // console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
+// console.log(assertEqual(eqObjects(shirtObject, longSleeveShirtObject)), false);
