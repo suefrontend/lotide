4399509f8b77e664dc4176883f1a456a1e94de44
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-05 23:44:31 -0700

    Complete assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,24 +1,36 @@
+const eqArrays = function (arr1, arr2) {
+  let len = arr1.length > arr2.length ? arr1.length : arr2.length;
+
+  for (let i = 0; i < len; i++) {
+    if (arr2.includes(arr1[i])) {
+      continue;
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function (object1, object2) {
-  //   const keys1 = Object.keys(object1);
-  //   const keys2 = Object.keys(object2);
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
-  //   if (!eqArrays(keys1, keys2)) {
-  //     return false;
-  //   }
+  if (!eqArrays(keys1, keys2)) {
+    return false;
+  }
 
-  //   for (const key of keys1) {
-  //     if (Array.isArray(object1[key])) {
-  //       if (!eqArrays(object1[key], object2[key])) {
-  //         return false;
-  //       }
-  //     } else {
-  //       if (object1[key] !== object2[key]) {
-  //         return false;
-  //       }
-  //     }
-  //   }
-  //   return true;
-  return object1 === object2;
+  for (const key of keys1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
 const assertObjectsEqual = function (actual, expected) {
@@ -26,13 +38,17 @@ const assertObjectsEqual = function (actual, expected) {
   const inspect = require("util").inspect; // <= add this line
 
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${expected}`);
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${expected}`);
+    console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
   }
 };
 
-console.log(
-  assertObjectsEqual(eqObjects({ a: "1", b: 2 }, { b: 2, a: "1" })),
-  true
-);
+const obj1 = { a: "1", b: 2 };
+const obj2 = { b: 2, a: "1" };
+
+console.log(assertObjectsEqual(obj1, obj2));
