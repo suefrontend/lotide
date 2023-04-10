7c8ec0093214f6ab6127ee595cfa0d2a83cb18d2
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-10 09:48:56 -0700

    Create test for tail.js function

--- tail.js
+++ tail.js
@@ -1,18 +1,7 @@
-const tail = function(arr) {
+const tail = function (arr) {
   if (arr.length <= 1) return [];
 
   return arr.slice(1);
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
